be860b27c4ad867181135dd054c687ee85595a6e
ZanRafal <01159225@pw.edu.pl>
2021-04-10 08:41:39 +0200

    Added buttons logic

--- app/src/main/java/com/example/easyenergysaver/Fragment4.java
+++ app/src/main/java/com/example/easyenergysaver/Fragment4.java
@@ -4,15 +4,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment4 extends Fragment {
+
+    public boolean globalVacationDay;
+    public int vacationDuration;
+    View view;
+    Button submit, reset;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment4_layout, container, false);
+        view = inflater.inflate(R.layout.fragment4_layout, container, false);
+
+        submit = (Button) view.findViewById(R.id.submitButton);
+        reset = (Button) view.findViewById(R.id.resetButton);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                globalVacationDay = true;
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                globalVacationDay = false;
+            }
+        });
+
+        return view;
     }
 }
