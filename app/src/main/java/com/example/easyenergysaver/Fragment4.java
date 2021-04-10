6d2c1a939df8482507160d389068f72052ef3dc9
ZanRafal <01159225@pw.edu.pl>
2021-04-10 10:04:23 +0200

    Visual changes in 2nd and 3rd layouts

--- app/src/main/java/com/example/easyenergysaver/Fragment4.java
+++ app/src/main/java/com/example/easyenergysaver/Fragment4.java
@@ -10,20 +10,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class Fragment4 extends Fragment {
 
     public boolean globalVacationDay;
-    public int vacationDuration;
+    public int varDur;
     View view;
     Button submit, reset;
+    TextInputEditText textInput;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment4_layout, container, false);
 
+        textInput = (TextInputEditText) view.findViewById(R.id.inputNumberOfDays);
+
+
         submit = (Button) view.findViewById(R.id.submitButton);
         reset = (Button) view.findViewById(R.id.resetButton);
+
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
