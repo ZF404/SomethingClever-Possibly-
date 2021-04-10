ad497ef2be9f0fa4b39df9361f88d988a0205b83
ZanRafal <01159225@pw.edu.pl>
2021-04-10 05:54:09 +0200

    Updating Frag3

--- app/src/main/java/com/example/easyenergysaver/Fragment3.java
+++ app/src/main/java/com/example/easyenergysaver/Fragment3.java
@@ -4,15 +4,56 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment3 extends Fragment {
+
+    private int count = 0;
+    public boolean isStateA, isStateB, isStateC, isStateD;
+    CheckBox stateA, stateB, stateC, stateD;
+    View view;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment3_layout, container, false);
+        view =  inflater.inflate(R.layout.fragment3_layout, container, false);
+
+
+        return view;
+    }
+
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.stateA:
+                if (checked)
+                    isStateA = true;
+            else
+                isStateA = false;
+                break;
+            case R.id.stateB:
+                if (checked)
+                    isStateB = true;
+            else
+                isStateB = false;
+                break;
+            case R.id.stateC:
+                if (checked)
+                    isStateC = true;
+            else
+                isStateC = false;
+                break;
+            case R.id.stateD:
+                if (checked)
+                    isStateD = true;
+            else
+                isStateD = false;
+                break;
+        }
     }
 }
