635f4bcb2c55a08cf55c8c997e8260dc31041c63
ZanRafal <01159225@pw.edu.pl>
2021-04-10 05:26:20 +0200

    Switch buttons in Frag2 working

--- app/src/main/java/com/example/easyenergysaver/Fragment2.java
+++ app/src/main/java/com/example/easyenergysaver/Fragment2.java
@@ -4,15 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment2 extends Fragment {
+
+    View view;
+    Switch electric,water;
+    public boolean isElectric, isWater;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment2_layout, container, false);
+        view = inflater.inflate(R.layout.fragment2_layout, container, false);
+
+        electric = (Switch) view.findViewById(R.id.eletcticHeating);
+        water = (Switch) view.findViewById(R.id.waterHeating);
+
+        electric.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) isElectric = true;
+                else isElectric = false;
+            }
+        });
+
+        water.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) isWater = true;
+                else isWater = false;
+            }
+        });
+
+        return view;
     }
 }
